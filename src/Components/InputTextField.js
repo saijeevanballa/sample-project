24f14c4a8cecc9b5c648aa2ea9a156bf1447eb07
Sai Jeevan Balla <saijeevanballa@gmail.com>
2022-12-04 15:45:43 +0530

    fix validations

--- src/Components/InputTextField.js
+++ src/Components/InputTextField.js
@@ -4,8 +4,15 @@ import { TextField, FormHelperText, Box } from '@mui/material'
 function InputTextField(props) {
   return (
     <Box width="200px" >
-      <TextField id="outlined-basic" label={props.propName} value={props.value} variant="outlined" />
-      <FormHelperText style={{marginLeft:"15px"}}>{props.helperText}</FormHelperText>
+      <TextField id="outlined-basic"
+        label={props.propName}
+        value={props.value}
+        name={props.name}
+        onBlur={props.onBlur}
+        onChange={props.onChange}
+        variant="outlined"
+      />
+      <FormHelperText style={{ marginLeft: "15px" }}>{props.helperText}</FormHelperText>
     </Box>
   )
 }
